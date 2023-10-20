15cbb75d2d3686c790acc2147f2a45c1145f881f
Coolkarni Mahesh <Coolkarni Mahesh@DESKTOP-96DTD7V>
2023-10-20 11:01:47 +0530

    Updated on 20-10-2023

---
+++ PSS.Hotel/Shared/SupervisorScreenModel.cs
@@ -0,0 +1,14 @@
+﻿namespace PSS.Hotel.Shared
+{
+    public class SupervisorScreenModel
+    {
+        public string? CaptainName { get; set; }
+        public string? WaiterName { get; set; }
+        public string? CustomerName { get; set; }
+        public string? TableName { get; set; }
+        public DateTime OrderedTime { get; set; }
+        public string? ItemName { get; set; }
+        public int PendingItemQty { get; set; }
+        public int Delivered { get; set; }
+    }
+}
